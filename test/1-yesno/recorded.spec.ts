7595c54ab177a3cb9d31a859d97963c35ec483ad
Ian Walker-Sperber <ianwsperber@gmail.com>
2019-03-11 16:49:21 -0700

    Instrumented test

--- test/1-yesno/recorded.spec.ts
+++ test/1-yesno/recorded.spec.ts
@@ -1,3 +1,4 @@
+import { yesno } from 'yesno-http';
 import Payments, { ICreateCustomer } from '../../src/client/payments';
 import * as testConfig from '../config';
 
@@ -8,6 +9,8 @@ interface IJSON {
 const FRAUD_DEVICE_INFO_TRIGGER_FAILURE = 'trigger-failure';
 
 describe('Payment SDK', () => {
+  const itRecorded = yesno.test({ it, dir: `${__dirname}/yesno`, prefix: 'payments' });
+
   let payments: Payments;
   let customerParams: ICreateCustomer;
 
@@ -21,8 +24,21 @@ describe('Payment SDK', () => {
   });
 
   describe('#createCustomer', () => {
-    it('should call all APIs and return the user object', async () => {
-      throw new Error('Not yet implemented');
+    itRecorded('should call all APIs and return the user object', async () => {
+      const customer = await payments.createCustomer(customerParams);
+
+      expect(yesno.intercepted()).toHaveLength(3);
+
+      expect((yesno.matching(/fraud/).response().body as IJSON).score).toBeGreaterThanOrEqual(0.5);
+      expect(customer.id).toEqual((yesno.matching(/users/).response().body as IJSON).id);
+
+      yesno.matching(/fraud/).redact(['request.headers.auth']);
+      yesno
+        .matching(/users/)
+        .redact(['request.headers.auth', 'request.body.email', 'response.body.email']);
+      yesno
+        .matching(/stripe/)
+        .redact(['request.headers.authorization', 'request.body', 'response.body.email']);
     });
   });
 });
