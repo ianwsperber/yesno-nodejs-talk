7cac70422378bd61f366f59ed178f43a7398411e
Ian Walker-Sperber <ianwsperber@gmail.com>
2019-03-11 14:38:09 -0700

    Remove all setup

--- test/1-yesno/recorded.spec.ts
+++ test/1-yesno/recorded.spec.ts
@@ -9,8 +9,6 @@ interface IJSON {
 const FRAUD_DEVICE_INFO_TRIGGER_FAILURE = 'trigger-failure';
 
 describe('Payment SDK', () => {
-  const itRecorded = yesno.test({ it, dir: `${__dirname}/yesno`, prefix: 'payments-sdk' });
-  const deviceInfo = 'foobar-device-info';
   let payments: Payments;
 
   beforeEach(() => {
@@ -18,25 +16,8 @@ describe('Payment SDK', () => {
   });
 
   describe('#createCustomer', () => {
-    itRecorded('should call all APIs and return the user object', async () => {
-      const user = await payments.createCustomer({
-        description: 'YesNo test user',
-        deviceInfo,
-        email: 'example@example.com',
-      });
-
-      expect(yesno.intercepted()).toHaveLength(3);
-
-      expect((yesno.matching(/fraud/).response().body as IJSON).score).toBeGreaterThanOrEqual(0.5);
-      expect(user.id).toEqual((yesno.matching(/users/).response().body as IJSON).id);
-
-      yesno.matching(/fraud/).redact(['request.headers.auth']);
-      yesno
-        .matching(/users/)
-        .redact(['request.headers.auth', 'request.body.email', 'response.body.email']);
-      yesno
-        .matching(/stripe/)
-        .redact(['request.headers.authorization', 'request.body', 'response.body.email']);
+    it('should call all APIs and return the user object', async () => {
+      throw new Error('Not yet implemented');
     });
   });
 });
