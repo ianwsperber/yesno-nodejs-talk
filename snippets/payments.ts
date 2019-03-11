44c4b99a901681c4ed2553597b2ca78674e4339b
Ian Walker-Sperber <ianwsperber@gmail.com>
2019-03-11 16:35:23 -0700

    More snippets

---
+++ snippets/payments.ts
@@ -0,0 +1,16 @@
+import Payments from '../src/client/payments';
+
+export async function sample() {
+  const payments = new Payments({
+    formidable: { baseUrl: 'example.com', secret: 'myformidablesecret' },
+    stripe: { secret: 'mysecret' },
+  });
+
+  const customer = await payments.createCustomer({
+    description: 'Test customer',
+    deviceInfo: 'foobar',
+    email: 'example@example.com',
+  });
+
+  console.log('Customer', customer);
+}
